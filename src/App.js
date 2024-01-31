49d7a87f296f6818892e7df7fdc87a06bec3fec5
Your Name <your.email@example.com>
2024-01-31 18:30:29 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css'
 
 export default function App() {
   return (
-    <div class="grid">
-      <div>One</div>
-      <div>Two</div>
-      <div>Three</div>
-      <div>Four</div>
+    <div class="container">
+      <div class="item first-row">A</div>
+      <div class="item first-row">B</div>
+      <div class="item first-row last-column">C</div>
+      <div class="item">D</div>
+      <div class="item">E</div>
+      <div class="item last-column">F</div>
+      <div class="item">G</div>
+      <div class="item">H</div>
+      <div class="item last-column">I</div>
     </div>
   )
 }
